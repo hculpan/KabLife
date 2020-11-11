92ee3608d715c16fea3fa1bc1414e6cd5dbc731e
Harry Culpan <harry@culpan.org>
2020-11-11 10:54:54 -0500

    Added start button so it doesn't auto start anymore

--- src/KabLife.cpp
+++ src/KabLife.cpp
@@ -44,9 +44,6 @@ EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 #define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
 #endif
 
-#define GRID_WIDTH 189
-#define GRID_HEIGHT 130
-
 class DemoApp
 {
 public:
@@ -60,10 +57,15 @@ public:
     void RunMessageLoop();
 
 private:
+    UINT GridWidth = 180;
+    UINT GridHeight = 120;
+
     BOOL* m_cell;
 
-    BOOL m_cell1[GRID_WIDTH * GRID_HEIGHT];
-    BOOL m_cell2[GRID_WIDTH * GRID_HEIGHT];
+    BOOL* m_cell1;
+    BOOL* m_cell2;
+
+    UINT m_iterationCount = 0;
 
     // Initialize device-independent resources.
     HRESULT CreateDeviceIndependentResources();
@@ -83,9 +85,13 @@ private:
         UINT height
     );
 
+    static void OnStartButton(DemoApp* pDemoApp);
+
     static void ProcessProc(void *ptr);
 
-    static UINT CountNeighbors(UINT x, UINT y, BOOL *cell);
+    UINT CountNeighbors(UINT x, UINT y, BOOL *cell);
+
+    inline UINT CellIndex(UINT x, UINT y);
 
     // The windows procedure.
     static LRESULT CALLBACK WndProc(
@@ -96,32 +102,34 @@ private:
     );
 
     HANDLE m_hRunMutex;
-    HWND m_hwnd;
+    HWND m_hwndParent;
+    HWND m_hwndRenderTarget;
+    HWND m_hwndStartButton;
+
+    // Direct2D objects
     ID2D1Factory* m_pDirect2dFactory;
     ID2D1HwndRenderTarget* m_pRenderTarget;
     ID2D1SolidColorBrush* m_pLightSlateGrayBrush;
     ID2D1SolidColorBrush* m_pCornflowerBlueBrush;
+
+    // Text objects
+    IDWriteFactory* m_pDWriteFactory;
+    IDWriteTextFormat* m_pTextFormat;
+    ID2D1Factory* m_pD2DFactory;
 };
 
 DemoApp::DemoApp() :
-    m_hwnd(NULL),
+    m_hwndParent(NULL),
     m_pDirect2dFactory(NULL),
     m_pRenderTarget(NULL),
     m_pLightSlateGrayBrush(NULL),
     m_pCornflowerBlueBrush(NULL)
 {
-    m_cell = &m_cell1[0];
+    m_cell1 = (BOOL*)(malloc(GridWidth * GridHeight * sizeof(BOOL)));
+    m_cell2 = (BOOL*)(malloc(GridWidth * GridHeight * sizeof(BOOL)));
 
-    m_hRunMutex = CreateMutexW(NULL, TRUE, NULL);      // Set
-    
-    for (int x = 0; x < GRID_WIDTH; x++) {
-        for (int y = 0; y < GRID_HEIGHT; y++) {
-            if (rand() % 100 > 50) m_cell[(y * GRID_WIDTH) + x] = TRUE;
-            else m_cell[(y * GRID_WIDTH) + x] = FALSE;
-        }
-    }
+    m_cell = NULL;
 }
-
 
 DemoApp::~DemoApp()
 {
@@ -130,6 +138,12 @@ DemoApp::~DemoApp()
     SafeRelease(&m_pLightSlateGrayBrush);
     SafeRelease(&m_pCornflowerBlueBrush);
 
+    free(m_cell1);
+    free(m_cell2);
+}
+
+UINT DemoApp::CellIndex(UINT x, UINT y) {
+    return (y * GridWidth) + x;
 }
 
 void DemoApp::RunMessageLoop()
@@ -173,24 +187,55 @@ HRESULT DemoApp::Initialize()
         UINT dpi = GetDpiForWindow(hDesktop);
 
         // Create the window.
-        m_hwnd = CreateWindow(
+        m_hwndParent = CreateWindow(
             L"KabLife",
             L"KabLife",
             WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
             CW_USEDEFAULT,
             CW_USEDEFAULT,
-            static_cast<UINT>(ceil(1272 * dpi / 96)),
-            static_cast<UINT>(ceil(895 * dpi / 96)),
+            static_cast<UINT>(ceil((GridWidth * 10) + 18)),
+            static_cast<UINT>(ceil((GridHeight * 10) + 10)),
             NULL,
             NULL,
             HINST_THISCOMPONENT,
             this
         );
-        hr = m_hwnd ? S_OK : E_FAIL;
+        hr = m_hwndParent ? S_OK : E_FAIL;
         if (SUCCEEDED(hr))
         {
-            ShowWindow(m_hwnd, SW_SHOWNORMAL);
-            UpdateWindow(m_hwnd);
+            m_hwndStartButton = CreateWindow(
+                L"BUTTON", 
+                L"Start", 
+                WS_TABSTOP | WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON, 
+                (GridWidth * 10) - 80, 
+                2, 
+                75, 
+                25, 
+                m_hwndParent, 
+                NULL, 
+                (HINSTANCE)GetWindowLongPtr(m_hwndParent, GWLP_HINSTANCE), 
+                NULL
+            );
+
+            wcex.lpszClassName = L"RenderTarget";
+            RegisterClassEx(&wcex);
+            m_hwndRenderTarget = CreateWindow(
+                L"RenderTarget",
+                L"render",
+                WS_CHILD | WS_VISIBLE,
+                0,
+                30,
+                GridWidth * 10,
+                GridHeight * 10 - 30,
+                m_hwndParent,
+                NULL,
+                (HINSTANCE)GetWindowLongPtr(m_hwndParent, GWLP_HINSTANCE),
+                NULL
+            );
+
+            ShowWindow(m_hwndParent, SW_SHOWNORMAL);
+            UpdateWindow(m_hwndParent);
+            InvalidateRect(m_hwndStartButton, NULL, FALSE);
         }
     }
 
@@ -204,6 +249,38 @@ HRESULT DemoApp::CreateDeviceIndependentResources()
     // Create a Direct2D factory.
     hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pDirect2dFactory);
 
+    if (SUCCEEDED(hr)) {
+        hr = DWriteCreateFactory(
+            DWRITE_FACTORY_TYPE_SHARED,
+            __uuidof(IDWriteFactory),
+            reinterpret_cast<IUnknown**>(&m_pDWriteFactory)
+        );
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        hr = m_pDWriteFactory->CreateTextFormat(
+            L"Arial",                // Font family name.
+            NULL,                       // Font collection (NULL sets it to use the system font collection).
+            DWRITE_FONT_WEIGHT_REGULAR,
+            DWRITE_FONT_STYLE_NORMAL,
+            DWRITE_FONT_STRETCH_NORMAL,
+            20.0f,
+            L"en-us",
+            &m_pTextFormat
+        );
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        hr = m_pTextFormat->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_LEADING);
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        hr = m_pTextFormat->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER);
+    }
+
     return hr;
 }
 
@@ -214,7 +291,7 @@ HRESULT DemoApp::CreateDeviceResources()
     if (!m_pRenderTarget)
     {
         RECT rc;
-        GetClientRect(m_hwnd, &rc);
+        GetClientRect(m_hwndRenderTarget, &rc);
 
         D2D1_SIZE_U size = D2D1::SizeU(
             rc.right - rc.left,
@@ -224,7 +301,7 @@ HRESULT DemoApp::CreateDeviceResources()
         // Create a Direct2D render target.
         hr = m_pDirect2dFactory->CreateHwndRenderTarget(
             D2D1::RenderTargetProperties(),
-            D2D1::HwndRenderTargetProperties(m_hwnd, size),
+            D2D1::HwndRenderTargetProperties(m_hwndRenderTarget, size),
             &m_pRenderTarget
         );
 
@@ -272,18 +349,18 @@ UINT DemoApp::CountNeighbors(UINT x, UINT y, BOOL *cell) {
     UINT result = 0;
 
     if (y > 0) {
-        if (x > 0 && cell[((y - 1) * GRID_WIDTH) + x - 1]) result++;
-        if (cell[((y - 1) * GRID_WIDTH) + x]) result++;
-        if (x + 1 < GRID_WIDTH && cell[((y - 1) * GRID_WIDTH) + x + 1]) result++;
+        if (x > 0 && cell[CellIndex(x - 1, y - 1)]) result++;
+        if (cell[((y - 1) * GridWidth) + x]) result++;
+        if (x + 1 < GridWidth && cell[((y - 1) * GridWidth) + x + 1]) result++;
     }
 
-    if (x > 0 && cell[(y * GRID_WIDTH) + x - 1]) result++;
-    if (x + 1 < GRID_WIDTH && cell[(y * GRID_WIDTH) + x + 1]) result++;
+    if (x > 0 && cell[(y * GridWidth) + x - 1]) result++;
+    if (x + 1 < GridWidth && cell[(y * GridWidth) + x + 1]) result++;
 
-    if (y + 1 < GRID_HEIGHT) {
-        if (x > 0 && cell[((y + 1) * GRID_WIDTH) + x - 1]) result++;
-        if (y > 0 && cell[((y + 1) * GRID_WIDTH) + x]) result++;
-        if (x + 1 < GRID_WIDTH && cell[((y + 1) * GRID_WIDTH) + x + 1]) result++;
+    if (y + 1 < GridHeight) {
+        if (x > 0 && cell[((y + 1) * GridWidth) + x - 1]) result++;
+        if (y > 0 && cell[((y + 1) * GridWidth) + x]) result++;
+        if (x + 1 < GridWidth && cell[((y + 1) * GridWidth) + x + 1]) result++;
     }
     return result;
 }
@@ -298,7 +375,9 @@ void DemoApp::ProcessProc(void *ptr)
 
     UINT neighbors;
 
+    pDemoApp->m_iterationCount = 0;
     do {
+        pDemoApp->m_iterationCount++; 
         if (pDemoApp->m_cell == &pDemoApp->m_cell1[0]) {
             oldGrid = &pDemoApp->m_cell1[0];
             newGrid = &pDemoApp->m_cell2[0];
@@ -308,16 +387,16 @@ void DemoApp::ProcessProc(void *ptr)
             newGrid = &pDemoApp->m_cell1[0];
         }
 
-        for (int x = 0; x < GRID_WIDTH; x++) {
-            for (int y = 0; y < GRID_HEIGHT; y++) {
-                neighbors = DemoApp::CountNeighbors(x, y, oldGrid);
-                BOOL alive = oldGrid[(y * GRID_WIDTH) + x];
+        for (int x = 0; x < pDemoApp->GridWidth; x++) {
+            for (int y = 0; y < pDemoApp->GridHeight; y++) {
+                neighbors = pDemoApp->CountNeighbors(x, y, oldGrid);
+                BOOL alive = oldGrid[(y * pDemoApp->GridWidth) + x];
 
                 if ((alive && (neighbors == 2 || neighbors == 3)) || (!alive && neighbors == 3)) {
-                    newGrid[(y * GRID_WIDTH) + x] = TRUE;
+                    newGrid[(y * pDemoApp->GridWidth) + x] = TRUE;
                 }
                 else {
-                    newGrid[(y * GRID_WIDTH) + x] = FALSE;
+                    newGrid[(y * pDemoApp->GridWidth) + x] = FALSE;
                 }
             }
         }
@@ -327,9 +406,24 @@ void DemoApp::ProcessProc(void *ptr)
         oldGrid = newGrid;
         newGrid = tempGrid;
 
-        InvalidateRect(pDemoApp->m_hwnd, NULL, FALSE);
+        InvalidateRect(pDemoApp->m_hwndParent, NULL, FALSE);
     }
     while (WaitForSingleObject(pDemoApp->m_hRunMutex, 75L) == WAIT_TIMEOUT);
+}
+
+void DemoApp::OnStartButton(DemoApp *pDemoApp) {
+    pDemoApp->m_cell = pDemoApp->m_cell1;
+
+    pDemoApp->m_hRunMutex = CreateMutexW(NULL, TRUE, NULL);
+
+    for (int x = 0; x < pDemoApp->GridWidth; x++) {
+        for (int y = 0; y < pDemoApp->GridHeight; y++) {
+            if (rand() % 100 > 50) pDemoApp->m_cell[pDemoApp->CellIndex(x, y)] = TRUE;
+            else pDemoApp->m_cell[pDemoApp->CellIndex(x, y)] = FALSE;
+        }
+    }
+
+    _beginthread(DemoApp::ProcessProc, 0, pDemoApp);
 }
 
 HRESULT DemoApp::OnRender()
@@ -339,6 +433,10 @@ HRESULT DemoApp::OnRender()
     hr = CreateDeviceResources();
     if (SUCCEEDED(hr))
     {
+        wchar_t wszText[20];
+        swprintf(wszText, 20, L"Iteration: %d", m_iterationCount);
+        UINT32 cTextLength_ = (UINT32)wcslen(wszText);
+
         BOOL* cell = m_cell;
 
         m_pRenderTarget->BeginDraw();
@@ -346,44 +444,59 @@ HRESULT DemoApp::OnRender()
         m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
         m_pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
 
-        D2D1_SIZE_F rtSize = m_pRenderTarget->GetSize();
-
         // Draw a grid background.
-        int width = static_cast<int>(rtSize.width);
-        int height = static_cast<int>(rtSize.height);
+        int width = GridWidth * 10 + 2;
+        int height = GridHeight * 10 + 2;
 
-        for (int x = 0; x < width; x += 10)
-        {
+        // Draw grid first
+        for (int x = 0; x <= GridWidth; x++) {
             m_pRenderTarget->DrawLine(
-                D2D1::Point2F(static_cast<FLOAT>(x + 1), 0.0f),
-                D2D1::Point2F(static_cast<FLOAT>(x + 1), rtSize.height),
+                D2D1::Point2F(static_cast<FLOAT>((x * 10) + 1), 0),
+                D2D1::Point2F(static_cast<FLOAT>((x * 10) + 1), height),
                 m_pLightSlateGrayBrush,
                 0.5f
             );
         }
 
-        for (int y = 0; y < height; y += 10)
-        {
+        for (int y = 0; y <= GridHeight; y++) {
             m_pRenderTarget->DrawLine(
-                D2D1::Point2F(0.0f, static_cast<FLOAT>(y + 1)),
-                D2D1::Point2F(rtSize.width, static_cast<FLOAT>(y + 1)),
+                D2D1::Point2F(0.0f, static_cast<FLOAT>((y * 10) + 1)),
+                D2D1::Point2F(width, static_cast<FLOAT>((y * 10) + 1)),
                 m_pLightSlateGrayBrush,
                 0.5f
             );
         }
 
-        for (int x = 0; x < GRID_WIDTH; x++) {
-            for (int y = 0; y < GRID_HEIGHT; y++) {
-                if (cell[(y * GRID_WIDTH) + x]) {
-                    D2D1_RECT_F rectangle1 = D2D1::RectF(
-                        (x * 10) + 2,
-                        (y * 10) + 2,
-                        (x * 10) + 10,
-                        (y * 10) + 10
-                    );
-                    m_pRenderTarget->FillRectangle(&rectangle1, m_pCornflowerBlueBrush);
+        if (m_cell) {
+            // Then draw cells
+            for (int x = 0; x < GridWidth; x++) {
+                for (int y = 0; y < GridHeight; y++) {
+                    if (cell[CellIndex(x, y)]) {
+                        D2D1_RECT_F rectangle1 = D2D1::RectF(
+                            (x * 10) + 2,
+                            (y * 10) + 2,
+                            (x * 10) + 10,
+                            (y * 10) + 10
+                        );
+                        m_pRenderTarget->FillRectangle(&rectangle1, m_pCornflowerBlueBrush);
+                    }
                 }
             }
+
+            D2D1_RECT_F layoutRect = D2D1::RectF(
+                10.0f,
+                0.0f,
+                300.0f,
+                30.0f
+            );
+
+            m_pRenderTarget->DrawText(
+                wszText,        // The string to render.
+                cTextLength_,    // The string's length.
+                m_pTextFormat,    // The text format.
+                layoutRect,       // The region of the window where the text will be rendered.
+                m_pCornflowerBlueBrush     // The brush used to draw the text.
+            );
         }
 
         hr = m_pRenderTarget->EndDraw();
@@ -414,9 +527,16 @@ LRESULT CALLBACK DemoApp::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM
             reinterpret_cast<LONG_PTR>(pDemoApp)
         );
 
-        _beginthread(DemoApp::ProcessProc, 0, pDemoApp);
-
         result = 1;
+    }
+    else if (message == WM_COMMAND) {
+          DemoApp* pDemoApp = reinterpret_cast<DemoApp*>(static_cast<LONG_PTR>(
+            ::GetWindowLongPtrW(
+                hwnd,
+                GWLP_USERDATA
+            )));
+
+        DemoApp::OnStartButton(pDemoApp);
     }
     else
     {
